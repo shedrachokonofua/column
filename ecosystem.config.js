474133a84c84f5a69adbceb1ada5dce81d97acee
Shedrach Okonofua <shedrachokonofua@gmail.com>
2019-01-19 20:20:57 -0500

    Initial Server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   apps : [{
+    name: 'Client',
+    script: 'npm',
+    args: 'run start-client',
+    exec_mode: 'cluster',
+    instances: 'max',
+  }, {
     name: 'Task Runner',
     script: './tasks/index.js',
     exec_mode: 'cluster',
