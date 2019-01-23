ec02b701f2608cd4fb90f5074ece8447ca484bfd
Shedrach Okonofua <shedrachokonofua@gmail.com>
2019-01-23 14:11:34 -0500

    Added date to email

--- crawlers/r/forhire/index.js
+++ crawlers/r/forhire/index.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const getLeads = require('./getLeads');
 const saveLead = require('./saveLead');
 const email = require('../../../modules/email/index');
@@ -10,6 +11,8 @@ module.exports = async function () {
     if(action) {
       const emailTemplate = await getEmailTemplate('r/forhire', 'newLead');
       lead.description = lead.description.replace(/\n/g, '<div></div>');
+      lead.fineDate = moment(lead.posted).fromNow();
+      
       await email({
         sender: 'r/forhire Crawler', 
         subject: `New Lead: ${lead.title}`, 
