25813caea4ecd3ee5ed7086c1da24bd1e57f6153
Shedrach Okonofua <shedrachokonofua@gmail.com>
2019-01-30 14:56:06 -0500

    Clear outdated leads from database daily

--- crawlers/r/forhire/config.js
+++ crawlers/r/forhire/config.js
@@ -1,13 +1,17 @@
 const crawl = require('./index')
+const removeOutdated = require('./removeOutdated')
 
 module.exports = {
   tasks: [
     {
       name: 'Find Dev Jobs',
-      fn: function(){
-        crawl()
-      },
-      cronTime: '*/5 * * * *'
+      fn: crawl,
+      cronTime: '*/5 * * * *' // Every 5 minutes
+    },
+    {
+      name: 'Clear Outdated Leads from Database',
+      fn: removeOutdated,
+      cronTime: '0 0 * * *' // Every day
     }
   ]
 }
--- modules/lead/model.js
+++ modules/lead/model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const moment = require('moment');
 const schema = require('./schema');
 
 module.exports = mongoose.model('Lead', schema);
